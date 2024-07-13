cde48d0b978fb16f7caac0f8cc338c6b362e2193
Anthony Nwadinobi <anthonynwadinobi@gmail.com>
2024-07-13 10:32:47 +0000

    ready for deployment

--- jobPortal/settings.py
+++ jobPortal/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)(34&0@vwna-y=_s!pvgz2=bvi&y#$l-u-k14a%k#v4h*diy13'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-jobs.herokuapp.com',
+                 'localhost',
+                 '8000-tochibot-djangojobs-dvh7wu7xp7u.ws-eu114.gitpod.io',
+                 '8000-tochibot-djangojobs-52g18y5hf7v.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -74,11 +81,15 @@ WSGI_APPLICATION = 'jobPortal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
