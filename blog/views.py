ce97de4e0e7c9d938378856fbc9b6cc6f8b5a3ea
Anthony Nwadinobi <anthonynwadinobi@gmail.com>
2024-07-22 11:21:40 +0000

    Set pagination to 9, and installed whitenoise

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ class BlogListView(ListView):
     queryset = BlogPost.objects.all()
     template_name = "blog/index.html"
     context_object_name = 'blog_list'
-    paginate_by = 6
+    paginate_by = 9
    
